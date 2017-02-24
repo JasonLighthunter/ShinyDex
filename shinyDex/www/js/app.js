c64fd69199c7d18c67028a1e42c31f5124690d02
Job Cuppen <jobcuppen@live.nl>
2017-02-24 14:59:27 +0100

    remove reference to tab-dashboard

--- shinyDex/www/js/app.js
+++ shinyDex/www/js/app.js
@@ -58,16 +58,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
   .state('tab.settings', {
     url: '/settings',
     views: {
@@ -79,5 +69,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/pokemon');
 });
