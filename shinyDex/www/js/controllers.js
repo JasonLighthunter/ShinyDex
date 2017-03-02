33853d92479a6086494b3d36e351ce99e653dd64
Job Cuppen <jobcuppen@live.nl>
2017-03-02 18:16:58 +0100

    can start camera from app on iOS.

--- shinyDex/www/js/controllers.js
+++ shinyDex/www/js/controllers.js
@@ -10,10 +10,45 @@ angular.module('starter.controllers', [])
 
 .controller('PokemonDetailCtrl', function($scope, $stateParams, Pokemon) {
   $scope.pokemon = Pokemon.get($stateParams.pokemonId);
+  $scope.test  = 'test';
 })
 
-.controller('SettingsCtrl', function($scope) {
+.controller('SettingsCtrl', function($scope, $ionicPlatform, $cordovaCamera) {
   $scope.settings = {
     enableFriends: true
+  };
+
+  $scope.label = "Hello World!";
+  $scope.changeButtonLabel = "Change Label";
+  $scope.cameraButtonLabel = 'take picture';
+
+  $scope.changeLabel = function() {
+    $scope.label = "Bye World";
+  };
+
+  $scope.takePicture = function() {
+
+    var options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 100,
+      targetHeight: 100,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: true,
+      correctOrientation: true
+    };
+
+    $cordovaCamera.getPicture(options).then(
+      function(imageData) {
+        var image = document.getElementById('myImage');
+        image.src = "data:image/jpeg;base64," + imageData;
+      }, 
+      function(err) {
+        console.log('Error Encountered');
+      }
+    );
   };
 });
